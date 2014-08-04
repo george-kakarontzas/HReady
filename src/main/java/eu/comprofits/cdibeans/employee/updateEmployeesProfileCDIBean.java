87fa5e60a1250670f89d87be405e6d7360cf4d14
George Kakarontzas <8199543+george-kakarontzas@users.noreply.github.com>
2014-08-04 22:31:01 +0300

    Added employee profile handling pages and related components. Added a few more dependencies in pom.xml

---
+++ src/main/java/eu/comprofits/cdibeans/employee/updateEmployeesProfileCDIBean.java
@@ -0,0 +1,229 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.comprofits.cdibeans.employee;
+
+import eu.comprofits.entities.employee.Employee;
+import eu.comprofits.entities.main.Department;
+import eu.comprofits.session.employee.EmployeeFacade;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.primefaces.model.UploadedFile;
+
+/**
+ *
+ * @author george
+ */
+@Named(value = "updateEmployeesProfileCDIBean")
+@SessionScoped
+public class updateEmployeesProfileCDIBean implements Serializable {
+
+    @EJB
+    private EmployeeFacade employeeFacade;
+    private Employee employee;
+    private List<Employee> employees;
+    private UploadedFile photograph;
+
+    /**
+     * Creates a new instance of UpdateOrganisationalPositionsCDIBean
+     */
+    public updateEmployeesProfileCDIBean() {
+    }
+
+    @PostConstruct
+    public void init() {
+        refreshDepartmentEmployeesList();
+    }
+
+    private void refreshDepartmentEmployeesList() {
+        Department d = this.getDepartment();
+        if (d != null) {
+            Collection coll = d.getEmployeeCollection();
+            if (coll instanceof List) {
+                employees = (List) coll;
+            } else {
+                employees = new ArrayList<>(coll);
+            }
+        } else {
+            employees = new ArrayList<>();
+        }
+    }
+
+    public Department getDepartment() {
+        //get department of currently logged in department head
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        Employee loggedInUser = (Employee) externalContext.getSessionMap().get("user");
+        if (loggedInUser != null) {
+            Department d = loggedInUser.getDepartmentIddepartment();
+            return d;
+        } //get Principal
+        else {
+            Principal principal
+                    = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+            if (principal != null) {
+                loggedInUser = employeeFacade.getEmployeeByUsername(principal.getName()); // Find User by j_username.
+                Department d = loggedInUser.getDepartmentIddepartment();
+                return d;
+            }
+        }
+        return null;
+    }
+
+    public String getRoleString(String roleAbbreviation) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msgs");
+        switch (roleAbbreviation) {
+            case "depthead":
+                return bundle.getString("depthead");
+
+            case "administrator":
+                return bundle.getString("administrator");
+
+            case "hrrecruiter":
+                return bundle.getString("hr_recruiter");
+
+            case "employee":
+                return bundle.getString("employee");
+
+            default:
+                return "";
+        }
+    }
+
+    public UploadedFile getPhotograph() {
+        return photograph;
+    }
+
+    public void setPhotograph(UploadedFile photograph) {
+        this.photograph = photograph;
+    }
+    
+    public String getPhotoPath() {
+        if (employee != null) {
+            if (employee.getPhotoPath()!=null) {
+                return "/images/"+employee.getPhotoPath();
+            }
+        }
+        return "/images/user.jpg";
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void upload() {
+        if (photograph != null) {
+            FacesMessage message = new FacesMessage("Succesful", photograph.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void remove(Employee e) {
+        try {
+            employeeFacade.remove(e);
+            refreshDepartmentEmployeesList();
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            ex.getMessage(), null));
+        }
+    }
+
+    public String edit(Employee e) {
+        this.employee = e;
+        return "editEmployeeProfile";
+    }
+
+    public String create() {
+        this.employee = new Employee();
+        employee.setDepartmentIddepartment(this.getDepartment());
+        return "editEmployeeProfile";
+    }
+
+    public String update() {
+        try {
+            //save photo of user if not null
+            if (photograph != null) {
+                File file = null;
+                OutputStream output = null;
+                try {
+                    // Prepare filename prefix and suffix for an unique filename in upload folder.
+                    String prefix = FilenameUtils.getBaseName(photograph.getFileName());
+                    String suffix = FilenameUtils.getExtension(photograph.getFileName());
+
+                    // Create file with unique name in upload folder and write to it.
+                    file = File.createTempFile(prefix + "_", "." + suffix, new File("c:/var/webapp/images"));
+                    output = new FileOutputStream(file);
+                    IOUtils.copy(photograph.getInputstream(), output);
+                    employee.setPhotoPath(file.getName());
+                    photograph=null;
+                    // Show succes message.
+                    FacesContext.getCurrentInstance().addMessage("uploadForm", new FacesMessage(
+                            FacesMessage.SEVERITY_INFO, "File upload succeed!", null));
+                } catch (IOException e) {
+                    // Cleanup.
+                    if (file != null) {
+                        file.delete();
+                    }
+
+                    // Show error message.
+                    FacesContext.getCurrentInstance().addMessage("uploadForm", new FacesMessage(
+                            FacesMessage.SEVERITY_ERROR, "File upload failed with I/O error.", null));
+
+                    // Always log stacktraces (with a real logger).
+                    e.printStackTrace();
+                } finally {
+                    IOUtils.closeQuietly(output);
+                }
+            }
+            String sha256hex = DigestUtils.sha256Hex(employee.getPassword());
+            employee.setPassword(sha256hex);
+            if (employee.getIdemployee() == null) {
+                employeeFacade.create(employee);
+            } else {
+                employeeFacade.edit(employee);
+            }
+            refreshDepartmentEmployeesList();
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            e.getMessage(), null));
+        }
+
+        return "updateEmployeeProfile";
+    }
+
+}
