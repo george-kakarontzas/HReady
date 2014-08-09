a025122fac3eb0de4a42290f039a835bab077a93
comprofits <comprofits@localhost>
2014-08-09 09:07:46 +0300

    Created the Managed Bean to veiw, select for update, update, create and remove employees profiessional experience records Create the page to view and remove employee professional experience records

---
+++ src/main/java/eu/comprofits/cdibeans/employee/updateProExperienceCDIBean.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.comprofits.cdibeans.employee;
+
+import eu.comprofits.entities.employee.Employee;
+import eu.comprofits.entities.employee.ProfessionalExperienceRecord;
+import eu.comprofits.session.employee.ProfessionalExperienceRecordFacade;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author george
+ */
+@Named(value = "updateProExperienceCDIBean")
+@SessionScoped
+public class updateProExperienceCDIBean implements Serializable {   
+    @EJB
+    private ProfessionalExperienceRecordFacade professionalExperienceRecordFacade;
+    
+    private Employee employee;
+    private ProfessionalExperienceRecord professionalExperienceRec;
+    private List<ProfessionalExperienceRecord> prorecs;
+   
+    
+    /**
+     * Creates a new instance of updateProExperienceCDIBean
+     */
+    public updateProExperienceCDIBean() {
+    }
+
+    @PostConstruct
+    public void init() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        employee = (Employee) externalContext.getSessionMap().get("employee");
+        refreshProRecsList();
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public List<ProfessionalExperienceRecord> getProrecs() {
+        return prorecs;
+    }
+ 
+    private void refreshProRecsList() {
+        prorecs = 
+            professionalExperienceRecordFacade.getProRecsForEmployee(employee);
+    }
+
+
+
+    public void remove(ProfessionalExperienceRecord p) {
+        try {
+            professionalExperienceRecordFacade.remove(p);           
+            refreshProRecsList();
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            ex.getMessage(), null));
+        }
+    }
+
+    public String edit(ProfessionalExperienceRecord p) {
+        this.professionalExperienceRec = p;
+        return "editProfessionalExperienceRecord";
+    }
+
+    public String create() {
+        this.professionalExperienceRec =
+                new ProfessionalExperienceRecord();
+        professionalExperienceRec.setEmployeeIdemployee(employee);
+        return "editProfessionalExperienceRecord";
+    }
+   
+    public String update() {
+        try {
+            if (professionalExperienceRec.getIdprofessionalExperienceRecord()==null) {
+               professionalExperienceRecordFacade.create(professionalExperienceRec);
+            } else {
+                professionalExperienceRecordFacade.edit(professionalExperienceRec);
+            }           
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            e.getMessage(), null));
+        }
+        refreshProRecsList();
+        return "updateProExperience";
+    }   
+}
