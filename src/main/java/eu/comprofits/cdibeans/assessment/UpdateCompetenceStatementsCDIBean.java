8ac325d6fcd9a8e6b81b583bbb3f5ac5902fc181
comprofits <comprofits@localhost>
2014-08-22 13:41:54 +0300

    Completed competence statements handling including capability to edit and delete existing statements as well as change their competence category.

--- src/main/java/eu/comprofits/cdibeans/assessment/UpdateCompetenceStatementsCDIBean.java
+++ src/main/java/eu/comprofits/cdibeans/assessment/UpdateCompetenceStatementsCDIBean.java
@@ -12,6 +12,7 @@ import eu.comprofits.session.main.CompetenceFacade;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -20,6 +21,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.RowEditEvent;
 
 /**
  *
@@ -78,8 +81,8 @@ public class UpdateCompetenceStatementsCDIBean implements Serializable {
 
     private void updateSelectedCompetenceStatements() {
         /*if the selectedCompetence is not null
-        update the statements list with the statements associated to
-        the selected competence */
+         update the statements list with the statements associated to
+         the selected competence */
         if (selectedCompetence != null) {
             selectedCompetenceStatements
                     = statementFacade.getCompetenceStatements(selectedCompetence);
@@ -87,7 +90,7 @@ public class UpdateCompetenceStatementsCDIBean implements Serializable {
     }
 
     public List<Statement> getSelectedCompetenceStatements() {
-        updateSelectedCompetenceStatements();
+        //updateSelectedCompetenceStatements();
         return selectedCompetenceStatements;
     }
 
@@ -110,17 +113,61 @@ public class UpdateCompetenceStatementsCDIBean implements Serializable {
     public void newStatementBtnPressed(ActionEvent event) {
         newStatement = new Statement();
         newStatement.setStatementText("");
+        RequestContext.getCurrentInstance().update("dialog");
     }
 
     public void saveNewStatement(ActionEvent event) {
         newStatement.setCompetenceId(selectedCompetence);
         try {
-            statementFacade.create(newStatement);   
-        }
-        catch (Exception e) {
-             FacesContext.getCurrentInstance().addMessage(null,
+            statementFacade.create(newStatement);
+            //update the selected competence statements
+            //since we've added a new competence
+            updateSelectedCompetenceStatements();
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_ERROR,
                             e.getMessage(), null));
+        }
+    }
+
+    public void onRowEdit(RowEditEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msgs");
+        try {
+            Statement i = (Statement) event.getObject();
+            System.out.println(i.getStatementText());
+            statementFacade.edit(i);
+            //since the statement may have changed category update the selected
+            //competence statements again
+            updateSelectedCompetenceStatements();
+            FacesMessage msg = new FacesMessage(bundle.getString("statement_edited"),
+                "" + ((Statement) event.getObject()).getIdstatement());
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+        catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            e.getMessage(), null));
+        }
+    }
+
+    public void onRowCancel(RowEditEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msgs");
+
+        FacesMessage msg = new FacesMessage(bundle.getString("statement_edit_canceled"),
+                "" + ((Statement) event.getObject()).getIdstatement());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void remove(Statement i) {
+        try {
+            statementFacade.remove(i);
+            updateSelectedCompetenceStatements();
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            ex.getMessage(), null));
         }
     }
 
