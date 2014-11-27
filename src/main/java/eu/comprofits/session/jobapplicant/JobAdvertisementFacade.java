f313f808ecfcd6b0bc20a2bf8c639c20ef74d9ba
Christos Kopanos <ckopanos@redmob.gr>
2014-11-27 17:20:28 +0200

    Applicants can apply for jobs
    Added the ability for an applicant to view available job positions, select one to view additional details and finally
    apply for the job.
    
    The applicant may also view his/her applications and remove the application
    
    Some minor fixes in the studies and professional experience form in the calendar field
    
    Changed job status field in the Job entity from Integer to Boolean to reflect the boolean field in the database

--- src/main/java/eu/comprofits/session/jobapplicant/JobAdvertisementFacade.java
+++ src/main/java/eu/comprofits/session/jobapplicant/JobAdvertisementFacade.java
@@ -8,9 +8,11 @@ package eu.comprofits.session.jobapplicant;
 
 import eu.comprofits.entities.jobapplicant.JobAdvertisement;
 import eu.comprofits.session.AbstractFacade;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,6 +30,13 @@ public class JobAdvertisementFacade extends AbstractFacade<JobAdvertisement> {
 
     public JobAdvertisementFacade() {
         super(JobAdvertisement.class);
+    }
+    
+    
+    public List<JobAdvertisement> getAvailableJobs() {
+        Query q = em.createQuery("SELECT ja FROM JobAdvertisement ja, Job jb WHERE ja.jobIdjob=jb AND  jb.status=:status");
+                q.setParameter("status", true);
+        return q.getResultList();
     }
     
 }
