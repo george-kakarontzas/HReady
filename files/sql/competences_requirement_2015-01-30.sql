73d8b50bc87547b0a22a6a08e4ca32b37443c4bd
Sascha Schneider <Sascha Schneider@localhost>
2015-02-09 00:02:03 +0100

    Returned weight and importance values from float to integer type to resolve the errors within EmployeeEvaluationCDIBean.java

--- files/sql/competences_requirement_2015-01-30.sql
+++ files/sql/competences_requirement_2015-01-30.sql
@@ -1,3 +1,3 @@
 ALTER TABLE competences_requirement DROP COLUMN weight;
 ALTER TABLE competences_requirement ADD COLUMN weight numeric(3,2);
-ALTER TABLE competences_requirement DROP COLUMN importance numeric(3,2);
+ALTER TABLE competences_requirement ADD COLUMN importance numeric(3,2);
