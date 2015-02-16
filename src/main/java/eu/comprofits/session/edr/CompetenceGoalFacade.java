35b5ca2fa5915ea5a46d7e16f72acda89e21f991
Sascha Schneider <Sascha Schneider@localhost>
2015-02-16 23:34:43 +0100

    EDRs are working again, but EdrNotes and EdrHistory are still broken, so I have disabled them for now.

--- src/main/java/eu/comprofits/session/edr/CompetenceGoalFacade.java
+++ src/main/java/eu/comprofits/session/edr/CompetenceGoalFacade.java
@@ -7,10 +7,17 @@
 package eu.comprofits.session.edr;
 
 import eu.comprofits.entities.edr.CompetenceGoal;
+import eu.comprofits.entities.edr.Edr;
+import eu.comprofits.entities.main.Competence;
 import eu.comprofits.session.AbstractFacade;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 /**
  *
@@ -30,4 +37,95 @@ public class CompetenceGoalFacade extends AbstractFacade<CompetenceGoal> {
         super(CompetenceGoal.class);
     }
     
+    public List<CompetenceGoal> getGoals() {
+        Query q = em.createQuery("SELECT c FROM CompetenceGoal c ORDER BY c.edrIdedr");
+        return q.getResultList();
+    }
+    
+    public List<CompetenceGoal> getGoalsForEdr(Edr edr) {
+        Query q = em.createQuery("SELECT c FROM CompetenceGoal c WHERE c.edrIdedr=:edr");
+        q.setParameter("edr",edr);
+        return q.getResultList();
+    }
+    
+    public CompetenceGoal getGoalForEdrAndCompetence(Edr edr,Competence com) {
+        Query q = em.createQuery("SELECT c FROM CompetenceGoal c WHERE c.edrIdedr=:edr AND c.competenceIdcompetence=:com");
+        q.setParameter("edr",edr);
+        q.setParameter("com",com);
+        CompetenceGoal c = null;
+        try {
+            c = (CompetenceGoal) q.getSingleResult();
+        } catch (NoResultException e) {}
+        return c;
+    }
+    
+    public TreeNode getCompetenceGoalsTree(TreeNode competencesTree, Edr edr)
+    {
+        TreeNode competenceGoalsTree = new DefaultTreeNode("Root",null);
+        
+        for (TreeNode c: competencesTree.getChildren())
+        {
+            getCompetenceGoalsTree(c,competenceGoalsTree, edr);
+        }
+        return competenceGoalsTree;
+    }
+    
+    public void getCompetenceGoalsTree(TreeNode childNode, TreeNode parentNode, Edr edr)
+    {       
+        if (this.getGoalForEdrAndCompetence(edr, (Competence)childNode.getData()) == null)
+        {
+            CompetenceGoal newCg = new CompetenceGoal();
+            newCg.setCompetenceIdcompetence((Competence)childNode.getData());
+            newCg.setEdrIdedr(edr);
+            newCg.setComments("");
+            newCg.setNextYearGoalValue(0);
+            TreeNode childTree = new DefaultTreeNode(newCg, parentNode);
+            
+            for (TreeNode c: childNode.getChildren())
+            {
+                getCompetenceGoalsTree(c, childTree, edr);
+            }
+        }
+        else
+        {
+            TreeNode childTree = new DefaultTreeNode(getGoalForEdrAndCompetence(edr, (Competence)childNode.getData()), parentNode);
+            for (TreeNode c: childNode.getChildren())
+            {
+                getCompetenceGoalsTree(c, childTree, edr);
+            }
+        }    
+    }
+    
+    public void updateCompetenceGoals (TreeNode competencesGoalsTree, Edr edr)
+    {
+        for (TreeNode cgNode : competencesGoalsTree.getChildren())
+        {
+            CompetenceGoal competenceGoal = (CompetenceGoal) cgNode.getData();
+            Competence competence = competenceGoal.getCompetenceIdcompetence();
+            if (getGoalForEdrAndCompetence(edr, competence) == null && (!competenceGoal.getComments().isEmpty() || competenceGoal.getNextYearGoalValue() != 0))
+            {
+                create(competenceGoal);
+            }
+            else
+            {
+                if (getGoalForEdrAndCompetence(edr, competence) != null && (!competenceGoal.getComments().isEmpty() || competenceGoal.getNextYearGoalValue() != 0))
+                {
+                    edit(competenceGoal);
+                }
+                else
+                {
+                    if (getGoalForEdrAndCompetence(edr, competence) != null && competenceGoal.getComments().isEmpty() && competenceGoal.getNextYearGoalValue() == 0)
+                    {
+                        remove(competenceGoal);
+                    }
+                }
+            }
+            updateCompetenceGoals(cgNode, edr);
+        }    
+    }
+    
+    public void removeCompetenceGoalsForEdr (Edr edr) {
+        Query q = em.createQuery("DELETE FROM CompetenceGoal WHERE edrIdedr=:edr");
+        q.setParameter("edr",edr);
+    }
 }
